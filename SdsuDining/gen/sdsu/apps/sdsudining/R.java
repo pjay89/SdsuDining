edfa0ee07f58ecad8882bb39a6578f2536b6785d
Priya Jayaprakash <pjay_89@hotmail.com>
2014-03-27 08:44:13 -0700

    Clickable home button Details View with tabs and fragments

--- SdsuDining/gen/sdsu/apps/sdsudining/R.java
+++ SdsuDining/gen/sdsu/apps/sdsudining/R.java
@@ -42,29 +42,48 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int sweet=0x7f020004;
     }
     public static final class id {
-        public static final int TableLayout1=0x7f090000;
-        public static final int TableLayout2=0x7f090002;
-        public static final int action_settings=0x7f09000b;
-        public static final int button1=0x7f09000a;
-        public static final int cateringButton=0x7f090007;
-        public static final int contactUsButton=0x7f090009;
-        public static final int couponsButton=0x7f090006;
-        public static final int farmersMarketButton=0x7f090005;
-        public static final int restaurantsButton=0x7f090004;
-        public static final int scrollView1=0x7f090001;
-        public static final int sweetButton=0x7f090008;
-        public static final int tableRow1=0x7f090003;
+        public static final int TableLayout1=0x7f090001;
+        public static final int TableLayout2=0x7f090003;
+        public static final int action_settings=0x7f09000d;
+        public static final int button1=0x7f09000b;
+        public static final int cateringButton=0x7f090008;
+        public static final int contactUsButton=0x7f09000a;
+        public static final int couponsButton=0x7f090007;
+        public static final int farmersMarketButton=0x7f090006;
+        public static final int pager=0x7f090000;
+        public static final int restaurantsButton=0x7f090005;
+        public static final int scrollView1=0x7f090002;
+        public static final int section_label=0x7f09000c;
+        public static final int sweetButton=0x7f090009;
+        public static final int tableRow1=0x7f090004;
     }
     public static final class layout {
-        public static final int activity_home=0x7f030000;
+        public static final int activity_details=0x7f030000;
+        public static final int activity_home=0x7f030001;
+        public static final int fragment_detail_dummy=0x7f030002;
+        public static final int fragment_details_a=0x7f030003;
+        public static final int fragment_details_b=0x7f030004;
+        public static final int fragment_details_c=0x7f030005;
     }
     public static final class menu {
-        public static final int home=0x7f080000;
+        public static final int detail=0x7f080000;
+        public static final int home=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
+        public static final int action_settings=0x7f060008;
         public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060002;
+        public static final int cateringString=0x7f060005;
+        public static final int contactUsString=0x7f060007;
+        public static final int couponsString=0x7f060004;
+        public static final int farmersMarketString=0x7f060003;
+        public static final int hello_blank_fragment=0x7f06000d;
+        public static final int home=0x7f060001;
+        public static final int restaurantsString=0x7f060002;
+        public static final int sweetString=0x7f060006;
+        public static final int title_activity_detail=0x7f060009;
+        public static final int title_section1=0x7f06000a;
+        public static final int title_section2=0x7f06000b;
+        public static final int title_section3=0x7f06000c;
     }
     public static final class style {
         /** 
@@ -86,13 +105,15 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         AppBaseTheme from BOTH res/values/styles.xml and
         res/values-v11/styles.xml on API 14+ devices.
     
- API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
+        /**  ActionBar tabs styles 
+         */
+        public static final int CustomTheme=0x7f070002;
     }
     public static final class styleable {
         /** 
--- SdsuDining/src/sdsu/apps/sdsudining/HomeActivity.java
+++ SdsuDining/src/sdsu/apps/sdsudining/HomeActivity.java
@@ -2,14 +2,28 @@ package sdsu.apps.sdsudining;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class HomeActivity extends Activity {
+
+	ImageButton restaurantsButton;
+	ImageButton farmersMarketButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		this.setTitle(R.string.home);
+
+		restaurantsButton = (ImageButton) findViewById(R.id.restaurantsButton);
+		restaurantsButton.setOnClickListener(getRestaurantButtonListner);
+		
+		farmersMarketButton = (ImageButton) findViewById(R.id.farmersMarketButton);
+		farmersMarketButton.setOnClickListener(getFarmersMarketButtonListener);
 	}
 
 	@Override
@@ -18,5 +32,27 @@ public class HomeActivity extends Activity {
 		getMenuInflater().inflate(R.menu.home, menu);
 		return true;
 	}
+
+	public OnClickListener getRestaurantButtonListner = new OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(HomeActivity.this, DetailsActivity.class);
+			intent.putExtra("labelString", getResources().getString(R.string.restaurantsString));
+			startActivity(intent);
+		}
+
+	};
+	
+	public OnClickListener getFarmersMarketButtonListener = new OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(HomeActivity.this, DetailsActivity.class);
+			intent.putExtra("labelString", getResources().getString(R.string.farmersMarketString));
+			startActivity(intent);
+		}
+
+	};
 
 }
